6b4be52102a28af3c15b8ab9414078cfd14354ac
Victor Lima <victor.lima@alicredito.com.br>
2020-07-31 16:00:28 -0300

    feat: remove autofill input on Chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,10 +5,14 @@ import { FormControl, InputLabel, OutlinedInput, InputAdornment, OutlinedInputPr
 type Props = OutlinedInputProps
 
 const Input: React.FC<Props> = (props: Props) => {
+  // Essa função enableInput é apenas um hack para impedir aquele auto-complete feio do Chrome
+  const enableInput = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    event.target.readOnly = false
+  }
   return (
     <FormControl className={Styles.formControl} variant="outlined">
-      <InputLabel htmlFor={props.id}>{props.label}</InputLabel>
-      <OutlinedInput {...props} labelWidth={70} endAdornment={<InputAdornment position="end">🔴</InputAdornment>} />
+      <InputLabel htmlFor={props.id} >{props.label}</InputLabel>
+      <OutlinedInput {...props} labelWidth={70} endAdornment={<InputAdornment position="end" >🔴</InputAdornment>} inputProps={{ readOnly: true }} onFocus={enableInput} />
     </FormControl>
   )
 }
