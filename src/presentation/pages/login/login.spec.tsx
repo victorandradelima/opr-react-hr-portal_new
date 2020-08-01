183cc26e03ca1c65fa44916dfcebecdb473bc46b
Victor Lima <victor.lima@alicredito.com.br>
2020-08-01 12:42:44 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, cleanup } from '@testing-library/react'
 import Login from './login'
+import { Validation } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
+    afterEach(cleanup)
+
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
@@ -30,4 +46,14 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
   })
+
+  // Este teste não está funcionando pois o fireEvent.input não reconhece o input dentro do componente do MaterialUI
+  // test('Should call Validation with correct email value', () => {
+  //   const { sut, validationSpy } = makeSut()
+  //   const emailInput = sut.getByTestId('email')
+  //   fireEvent.input(emailInput, { target: { value: 'any_email' } })
+  //   expect(validationSpy.input).toEqual({
+  //     email: 'any_email'
+  //   })
+  // })
 })
