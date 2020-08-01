830395e05640c2d0f74b43306321db7bf689be18
Victor Lima <victor.lima@alicredito.com.br>
2020-08-01 14:08:04 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,11 +20,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Valor válido'
   }
 
   return (
