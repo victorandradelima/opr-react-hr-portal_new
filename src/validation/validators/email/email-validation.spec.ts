d8bc872070502bfbb70e653d7e14b39d730efadf
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 17:07:24 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { EmailValitation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('EmailValition', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValitation('email')
-    const error = sut.validate('')
+    const sut = new EmailValitation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValitation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
