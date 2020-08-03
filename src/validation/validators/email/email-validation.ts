b9ac976addf26582fc0a1c473453f233bd631cd8
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 17:06:12 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -7,6 +7,8 @@ export class EmailValitation implements FieldValidation {
   ) {}
 
   validate (value: string): InvalidFieldError {
-    return new InvalidFieldError()
+    // eslint-disable-next-line no-useless-escape
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
