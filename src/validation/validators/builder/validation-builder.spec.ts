409ea78549de15ee57f9cabe53503370a31aec60
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 20:20:05 -0300

    test: ensure ValidationBuider has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,11 +1,19 @@
 import { RequiredFieldValidation } from '@/validation/validators'
-import { ValidationBuilder } from './validation-builder'
+import { ValidationBuilder as sut } from './validation-builder'
+import { EmailValitation } from '../email/email-validation'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = ValidationBuilder.field('any_field').required().build()
+    const validations = sut.field('any_field').required().build()
     expect(validations).toEqual(
       [new RequiredFieldValidation('any_field')]
+    )
+  })
+
+  test('Should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build()
+    expect(validations).toEqual(
+      [new EmailValitation('any_field')]
     )
   })
 })
