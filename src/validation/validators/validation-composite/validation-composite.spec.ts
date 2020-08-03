8789119eb2170156f22e2832a452ea93d136be66
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 19:19:39 -0300

    test: ensure ValidationComposite returns the first error found

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,17 @@
+import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
+import { ValidationComposite } from './validation-composite'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails and return just first error', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('second_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error_message')
+  })
+})
