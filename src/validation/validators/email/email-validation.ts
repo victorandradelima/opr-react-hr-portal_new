ac5e592d86e519b67ac0a54a20aab85d3fa5fe37
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 18:43:41 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -9,6 +9,6 @@ export class EmailValitation implements FieldValidation {
   validate (value: string): InvalidFieldError {
     // eslint-disable-next-line no-useless-escape
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return emailRegex.test(value) ? null : new InvalidFieldError()
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError()
   }
 }
