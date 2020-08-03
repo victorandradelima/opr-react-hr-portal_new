b9f2d7d6892fa54981890d8c817c52c7c55eaf8f
Victor Lima <victor.lima@alicredito.com.br>
2020-08-03 20:26:22 -0300

    feat: ensure ValidationBuider has a minLength  helper

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,7 +1,7 @@
 import { FieldValidation } from '@/validation/protocols/field-validation'
 import { InvalidFieldError } from '@/validation/errors'
 
-export class MinLenghtValidation implements FieldValidation {
+export class MinLengthValidation implements FieldValidation {
   constructor (
     readonly field: string,
     private readonly minLength: number
