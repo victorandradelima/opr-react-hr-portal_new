d1e65de397ad0cb9eb3b7126b876b324637ae53d
Victor Lima <victor.lima@alicredito.com.br>
2020-08-18 17:26:45 -0300

    test: ensure Login present error if SaveAccessToken throws

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -185,6 +185,15 @@ describe('Login Component', () => {
     expect(history.location.pathname).toBe('/')
   })
 
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error))
+    await simulateValidSubmit(sut)
+    testElementText(sut, 'main-error', error.message)
+    testErrorWrapChildCount(sut, 1)
+  })
+
   // Este teste serve para verificar a navegação para uma página de signup
   // test('Should go to signup page', () => {
   //   const { sut } = makeSut()
